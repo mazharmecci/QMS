16f1e7498c36142736352f89840c73a19e6dfad0
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-12-22 16:43:29 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,12 @@
+// ../js/supabase.js
 const SUPABASE_URL = "https://kkzvniasiqfoswdatjvm.supabase.co";
 const SUPABASE_KEY = "sb_publishable_4t-uExEPlZ81VgcZoZp43w_NusdpCDj";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Get createClient from the global supabase object provided by the CDN
+const { createClient } = supabase;
 
+// Create one client instance
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Expose it globally so other scripts can use `supabase`
 window.supabase = supabaseClient;
