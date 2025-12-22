4de9ade96623f0ee1691d6ee4ab20b58c068b084
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-12-22 16:40:17 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,6 @@
 const SUPABASE_URL = "https://kkzvniasiqfoswdatjvm.supabase.co";
 const SUPABASE_KEY = "sb_publishable_4t-uExEPlZ81VgcZoZp43w_NusdpCDj";
 
-const supabase = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+window.supabase = supabaseClient;
