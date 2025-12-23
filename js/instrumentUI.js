f6864ee431be17cf5812ab08f8dfc183e74fa834
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-12-23 16:48:55 +0530

    Update instrumentUI.js

--- js/instrumentUI.js
+++ js/instrumentUI.js
@@ -4,7 +4,7 @@ import {
   addInstrument,
   updateInstrument,
   deleteInstrument
-} from "./instrumentService.js";
+} from "../js/instrumentService.js";
 
 const form = document.getElementById("instrumentForm");
 const tableBody = document.querySelector("#instrumentTable tbody");
